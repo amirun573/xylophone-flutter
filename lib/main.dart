bd4e9f26c508223ecfb73148693f06d28e4a0d6f
amirun573 <amirunaqmar@gmail.com>
2021-01-15 20:23:35 +0800

    Added Sound to Button

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,13 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(child: Text(nouns.first)),
+          child: Center(
+              child: FlatButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note1.wav')
+                  },
+                  child: Text('Clicked Me'))),
         ),
       ),
     );
